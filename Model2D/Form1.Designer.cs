be3ed02df648f4303048ed9b7e8d8765cfb291b9
fhdavidqqq@gmail.com <fhdavidqqq@gmail.com>
2022-05-15 20:23:30 +0300

    bag fix

---
+++ Model2D/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Model2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(747, 351);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(747, 291);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "70";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(747, 321);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "65";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(722, 291);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(722, 324);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Model2D.Properties.Resources.Безымянный;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(561, 367);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 392);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Model2D/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+
+namespace Model2D
+{
+    public partial class Form1 : Form
+    {
+        private SldWorks swApp;
+        private ModelDoc2 swModel;
+        private SketchManager swSketchManager;
+        private SelectionMgr swSelMgr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Если не получилось открыть деталь в SolidWorks то завершить
+            if (!GetSolidworks())
+            {
+                return;
+            }
+
+            double x0 = 100.0 / 1000.0;
+            double y0 = 120.0 / 1000.0;
+
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text) / 1000;
+            b = Convert.ToDouble(textBox2.Text) / 1000;
+            var line = swModel.SketchManager.CreateLine(x0, y0, 0, x0, y0 + b, 0).GetSketch().GetSketchPoints2();
+            line = swModel.SketchManager.CreateLine(
+                line[1].X,
+                line[1].Y, 0,
+                line[1].X + 0.020,
+                line[1].Y, 0).GetSketch().GetSketchPoints2();
+            line = swModel.SketchManager.CreateLine(
+                line[2].X,
+                line[2].Y, 0,
+                line[2].X,
+                line[2].Y - 0.035, 0).GetSketch().GetSketchPoints2();
+            line = swModel.SketchManager.CreateLine(
+                line[3].X,
+                line[3].Y, 0,
+                line[3].X + 0.020,
+                line[3].Y, 0).GetSketch().GetSketchPoints2();
+            line = swModel.SketchManager.CreateLine(
+                line[4].X,
+                line[4].Y, 0,
+                line[4].X,
+                line[4].Y + 0.035, 0).GetSketch().GetSketchPoints2();
+            swModel.SketchManager.CreateLine(
+                line[5].X,
+                line[5].Y, 0,
+                line[5].X + 1,
+                line[5].Y, 0).GetSketch().GetSketchPoints2();
+
+            swModel.SketchManager.CreateCircleByRadius(x0 + a, y0 + b - 0.020, 0, 0.015);
+            swModel.SketchManager.CreateArc(x0 + a, y0 + b - 0.020, 0, x0 + a, y0 + b - 0.020 + 0.025, 0, x0 + a, y0 + b - 0.020 - 0.025, 0, -1);
+            swModel.SketchManager.CreateLine(x0 + a - 0.015, y0 + b - 0.040, 0, x0 + 1, y0 + b - 0.040, 0);
+            swModel.SketchManager.CreateCircleByRadius(x0 + 0.020, y0, 0, 0.015);
+
+            swModel.SketchManager.CreateArc(x0 + 0.020, y0, 0, x0 + 0.020 - 0.020, y0, 0, x0 + 0.020 + 0.020, y0, 0, 1);
+            swModel.SketchManager.CreateLine(x0 + a - 0.015, y0 + b - 0.040, 0, x0 + 0.020, y0 - 0.020, 0);
+
+            swModel.Extension.SelectByID2("Line7", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Arc4", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchAddConstraints("sgFIXED");
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Arc4", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line8", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchAddConstraints("sgTANGENT");
+            swModel.ClearSelection2(true);
+            //swModel.Extension.SelectByID2("Line8", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            //swModel.SketchConstraintsDel(0, "sgFIXED");
+            //swModel.ClearSelection2(true);
+
+            swModel.Extension.SelectByID2("Line8", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.Extension.SelectByID2("Arc4", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.SketchManager.SketchTrim(0, x0 + 0.020, y0 + 0.020, 0);
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Line6", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.Extension.SelectByID2("Arc2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.SketchManager.SketchTrim(0, x0 + a, y0 + b, 0);
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Line7", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.Extension.SelectByID2("Arc2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.SketchManager.SketchTrim(0, x0 + a, y0 + b - 0.040, 0);
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Arc2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line6", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.SketchTrim(0, x0 + 1, y0 + b, 0);
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Arc2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line7", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.SketchTrim(0, x0 + 1, y0 + b - 0.040, 0);
+            swModel.ClearSelection2(true);
+        }
+
+        private Boolean GetSolidworks()
+        {
+            try
+            {
+                // Присваиваем переменной ссылку на запущенный solidworks (по названию)
+                swApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            }
+            catch
+            {
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show("Не удалось найти запущенный Solidworks!");
+                return false;
+            }
+
+            if (swApp.ActiveDoc == null)
+            {
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show("Надо открыть деталь перед использованием");
+                return false;
+            }
+
+
+            // Присваиваем переменной ссылку на открытый активный проект в  SolidWorks
+            swModel = (ModelDoc2)swApp.ActiveDoc;
+
+            // Получает ISketchManager объект, который позволяет получить доступ к процедурам эскиза
+            swSketchManager = (SketchManager)swModel.SketchManager;
+
+            // Получает ISelectionMgr объект для данного документа, что делает выбранный объект доступным
+            swSelMgr = (SelectionMgr)swModel.SelectionManager;
+
+
+            // Проверка на открытие именно детали в SolidWorks
+            if (swModel.GetType() != (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                string text = "Это работает только на уровне чертежа";
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show(text, "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
